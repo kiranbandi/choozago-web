50ddb4ffdb6342aff0e0e13dc1db6d4516b43166
kiranbandi <bvenkatkiran@gmail.com>
2017-01-10 20:32:07 +0530

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './js/main.js',
+  output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+  devServer: {
+    inline: true,
+    contentBase: './',
+    port: 8888
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+       { test: /\.css$/, 
+        loader: "style!css" 
+    }
+    ]
+  }
+}
+
