306b8e32c445d47ac3b5568c122db58b7ae5c93f
bandi kiran <bvenkatkiran@gmail.com>
2017-01-18 10:20:48 +0000

    added a simple login modal

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
-import { NavBar } from './';
+import { NavBar, Modal } from './';
 
 export default class Container extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            'view': { 'showModal': false }
+        };
+        this.handleHideModal=this.handleHideModal.bind(this);
+        this.handleShowModal=this.handleShowModal.bind(this);
+    }
+
+    handleHideModal(){
+        this.setState({view: {showModal: false}})
+    }
+    handleShowModal(){
+        this.setState({view: {showModal: true}})
     }
     
   	render() {
 		return (
 		      <div id='app-container'>
-                    {/* navbar content , common for entire application */}
-                    <NavBar/>
-                    {this.props.children}
-              </div>
+                {/* navbar content , common for entire application */}
+                <NavBar handleShowModal={this.handleShowModal}/>
+                {this.props.children}
+                {/*Modal Content part of main Container*/}
+                <div className="row">
+                    <button className="btn btn-default btn-block hide" onClick={this.handleShowModal}>Open Modal</button>
+                    {this.state.view.showModal ? <Modal handleHideModal={this.handleHideModal}/> : null}
+                </div>
+          </div>
 		);
 	}
 }  
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as TicketCard } from './TicketCard';
+export { default as Modal } from './Modal';
 export { default as NavBar } from './NavBar';
 export { default as Container } from './Container';
+
